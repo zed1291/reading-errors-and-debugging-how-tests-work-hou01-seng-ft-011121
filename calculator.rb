becf2f2a01d89aa194aacdd37e1520b63834b5e1
zed <zed@kolk.me>
2020-12-11 19:25:45 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,6 @@
-# Add your variables here
+first_number = 0
+second_number = first_number + 1
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
